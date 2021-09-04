6c82825820272079e823bbe3fe7e31244b6467f5
lramos6 <lramos6@students.towson.edu>
2021-09-03 20:43:32 -0400

    chore: create color objects class

---
+++ data/color.py
@@ -0,0 +1,16 @@
+"""Predefined Color objects class"""
+from rgbmatrix import graphics
+
+
+class Color:
+    RED = graphics.Color(171, 0, 3)
+    ORANGE = graphics.Color(128, 128, 128)
+    YELLOW = graphics.Color(239, 178, 30)
+    GREEN = graphics.Color(124, 252, 0)
+    BLUE = graphics.Color(0, 45, 114)
+    PURPLE = graphics.Color(51, 0, 111)
+    PINK = graphics.Color(255, 143, 255)
+    BROWN = graphics.Color(65, 29, 0)
+    GRAY = graphics.Color(196, 206, 212)
+    BLACK = graphics.Color(0, 0, 0)
+    WHITE = graphics.Color(255, 255, 255)
