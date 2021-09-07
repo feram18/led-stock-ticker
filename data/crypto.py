4157ed840646c08730dc9c556e3bf91f1e937880
Luis Ramos <feram99ag@gmail.com>
2021-09-07 18:44:50 -0400

    refactor: remove redundant method

--- data/crypto.py
+++ data/crypto.py
@@ -7,27 +7,14 @@ class Crypto(Ticker):
 
     def get_name(self) -> str:
         """
-        Fetch crypto's full name. i.e. BTC -> Bitcoin.
+        Fetch crypto's full name. i.e. BTC -> Bitcoin. Removes 'USD' suffix.
         :return: name: (str) Name
         :exception KeyError: If incorrect data type is provided as an argument. Can occur when a ticker is not valid.
         """
         try:
-            return self.format_name(self.data.info['shortName'])
+            name = self.data.info['shortName']
+            return name.replace(' USD', '')
         except KeyError:
             self.valid = False
             self.update_status = Status.FAIL
             return ''
-
-    @staticmethod
-    def format_name(name: str) -> str:
-        """
-        Format crypto's name string to remove currency from it.
-        i.e. Bitcoin USD -> Bitcoin
-        :param name: (str) Name to format
-        :return: name: (str) Formatted name
-        """
-        currency_postfix = ' USD'
-        if currency_postfix in name.upper():
-            return name.replace(currency_postfix, '')
-        else:
-            return name
