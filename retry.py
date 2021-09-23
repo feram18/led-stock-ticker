5a9ef6fb8fefd638ad6d7b6f8d528bbe84eb21f2
Luis Ramos <feram99ag@gmail.com>
2021-09-23 00:23:59 -0400

    feat(retry): add retry by FBolser

---
+++ retry.py
@@ -0,0 +1,67 @@
+# Copyright 2021 Fabian Bosler
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+# Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+# THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFINGEMENT. IN NO EVENT SHALL THE AUTHORS
+# OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+from functools import wraps
+import time
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def retry(exceptions, total_tries=4, initial_wait=0.5, backoff_factor=2, logger=None):
+    """
+    calling the decorated function applying an exponential backoff.
+    Args:
+        exceptions: Exception(s) that trigger a retry, can be a tuple
+        total_tries: Total tries
+        initial_wait: Time to first retry
+        backoff_factor: Backoff multiplier (e.g. value of 2 will double the delay each retry).
+        logger: logger to be used, if none specified print
+    """
+
+    def retry_decorator(f):
+        @wraps(f)
+        def func_with_retries(*args, **kwargs):
+            _tries, _delay = total_tries + 1, initial_wait
+            while _tries > 1:
+                try:
+                    log(f'{total_tries + 2 - _tries}. try:', logger)
+                    return f(*args, **kwargs)
+                except exceptions as e:
+                    _tries -= 1
+                    print_args = args if args else 'no args'
+                    if _tries == 1:
+                        msg = str(f'Function: {f.__name__}\n'
+                                  f'Failed despite best efforts after {total_tries} tries.\n'
+                                  f'args: {print_args}, kwargs: {kwargs}')
+                        log(msg, logger)
+                        raise
+                    msg = str(f'Function: {f.__name__}\n'
+                              f'Exception: {e}\n'
+                              f'Retrying in {_delay} seconds!, args: {print_args}, kwargs: {kwargs}\n')
+                    log(msg, logger)
+                    time.sleep(_delay)
+                    _delay *= backoff_factor
+
+        return func_with_retries
+
+    return retry_decorator
+
+
+def log(msg, logger=None):
+    if logger:
+        logger.warning(msg)
+    else:
+        print(msg)
