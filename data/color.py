f07dbd69486967d2215a952631f0ab2c639250bd
Luis Ramos <feram99ag@gmail.com>
2021-10-23 22:50:55 -0400

    refactor: rename import alias RGB_Color -> RGB

--- data/color.py
+++ data/color.py
@@ -1,17 +1,17 @@
-from rgbmatrix.graphics import Color as RGB_Color
+from rgbmatrix.graphics import Color as RGB
 
 
 class Color:
     """Predefined Color objects class"""
 
-    RED = RGB_Color(171, 0, 3)
-    ORANGE = RGB_Color(128, 128, 128)
-    YELLOW = RGB_Color(239, 178, 30)
-    GREEN = RGB_Color(124, 252, 0)
-    BLUE = RGB_Color(0, 45, 114)
-    PURPLE = RGB_Color(51, 0, 111)
-    PINK = RGB_Color(255, 143, 255)
-    BROWN = RGB_Color(65, 29, 0)
-    GRAY = RGB_Color(196, 206, 212)
-    BLACK = RGB_Color(0, 0, 0)
-    WHITE = RGB_Color(255, 255, 255)
+    RED = RGB(171, 0, 3)
+    ORANGE = RGB(128, 128, 128)
+    YELLOW = RGB(239, 178, 30)
+    GREEN = RGB(124, 252, 0)
+    BLUE = RGB(0, 45, 114)
+    PURPLE = RGB(51, 0, 111)
+    PINK = RGB(255, 143, 255)
+    BROWN = RGB(65, 29, 0)
+    GRAY = RGB(196, 206, 212)
+    BLACK = RGB(0, 0, 0)
+    WHITE = RGB(255, 255, 255)
