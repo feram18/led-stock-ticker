2db1cc347ccb2f36982757240f43da3c6c2a392b
Luis Ramos <feram99ag@gmail.com>
2022-10-10 14:35:59 -0400

    refactor: import holidays & rename holiday name

--- util/holiday_calendar.py
+++ util/holiday_calendar.py
@@ -1,6 +1,5 @@
-from pandas.tseries.offsets import WeekOfMonth
-from pandas.tseries.holiday import AbstractHolidayCalendar, Holiday, sunday_to_monday, GoodFriday, USMemorialDay, \
-    USLaborDay, USThanksgivingDay
+from pandas.tseries.holiday import AbstractHolidayCalendar, Holiday, nearest_workday, USMartinLutherKingJr, \
+    USPresidentsDay, GoodFriday, USMemorialDay, USLaborDay, USThanksgivingDay
 
 
 class MarketHolidayCalendar(AbstractHolidayCalendar):
@@ -9,32 +8,14 @@ class MarketHolidayCalendar(AbstractHolidayCalendar):
     Based on holidays listed at https://www.nyse.com/markets/hours-calendars
     """
     rules = [
-        Holiday("New Year's Day",
-                month=1,
-                day=1,
-                observance=sunday_to_monday),
-        Holiday('Martin Luther King, Jr. Day',
-                month=1,
-                day=1,
-                offset=WeekOfMonth(week=3, weekday=1)),
-        Holiday("Washington's Birthday",
-                month=2,
-                day=1,
-                offset=WeekOfMonth(week=3, weekday=1)),
+        Holiday("New Year's Day", month=1, day=1, observance=nearest_workday),
+        USMartinLutherKingJr,
+        USPresidentsDay,
         GoodFriday,
         USMemorialDay,
-        Holiday('Juneteenth',
-                month=6,
-                day=19,
-                observance=sunday_to_monday),
-        Holiday('Independence Day',
-                month=7,
-                day=4,
-                observance=sunday_to_monday),
+        Holiday('Juneteenth National Independence Day', month=6, day=19, observance=nearest_workday),
+        Holiday('Independence Day', month=7, day=4, observance=nearest_workday),
         USLaborDay,
         USThanksgivingDay,
-        Holiday('Christmas Day',
-                month=12,
-                day=25,
-                observance=sunday_to_monday),
+        Holiday('Christmas Day', month=12, day=25, observance=nearest_workday),
     ]
