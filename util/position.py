1728f488d418e73c6a841fb3fec6d64d7d087123
Luis Ramos <feram99ag@gmail.com>
2022-10-10 14:35:21 -0400

    refactor: enum values

--- util/position.py
+++ util/position.py
@@ -2,8 +2,9 @@ from enum import Enum
 
 
 class Position(Enum):
-    """Enum class for positioning on matrix' canvas"""
-    TOP = 0
-    RIGHT = 1
-    CENTER = 2
-    BOTTOM = 3
+    """Enum class for positioning on canvas"""
+    TOP = 'top'
+    RIGHT = 'right'
+    CENTER = 'center'
+    BOTTOM = 'bottom'
+    LEFT = 'left'
