9d8d44993038735017a0079a473669327200924b
Luis Ramos <feram99ag@gmail.com>
2022-10-13 18:58:23 -0400

    refactor: add crypto logo url constant

--- constants.py
+++ constants.py
@@ -9,8 +9,21 @@ ERROR_IMAGE = 'assets/img/error.png'
 FONTS_DIR = 'assets/fonts/'
 
 # Software Defaults
-DEFAULT_STOCKS = ['TSLA', 'AMZN', 'MSFT']
-DEFAULT_CRYPTOS = ['BTC', 'ETH', 'LTC']
+DEFAULT_STOCKS = [
+    'TSLA',
+    'AMZN',
+    'MSFT'
+]
+DEFAULT_CRYPTOS = [
+    'BTC',
+    'ETH',
+    'LTC'
+]
+DEFAULT_FOREX = [
+    'USD/EUR',
+    'EUR/JPY',
+    'GBP/USD'
+]
 DEFAULT_DATE_FORMAT = '%a, %b %d'  # eg. Sun, Jan 5
 DEFAULT_UPDATE_RATE = 10 * 60  # 10 minutes
 DEFAULT_ROTATION_RATE = 10  # seconds
@@ -22,6 +35,9 @@ EXCHANGE_RATE_BASE_URL = 'https://api.exchangerate.host/'
 CURRENCY_EXCHANGE_URL = EXCHANGE_RATE_BASE_URL + 'convert?from={}&to={}&amount={}&places=2'
 FOREX_RATES_URL = EXCHANGE_RATE_BASE_URL + 'fluctuation?start_date={}&end_date={}&base={}&symbols={}&places=4'
 
+# Image sources
+CRYPTO_LOGO_URL = 'https://cryptoicons.org/api/icon/{}/200'
+
 # Date/Time Formatting
 DATE_FORMATS = [
     '%a, %b %d',  # Sun, Jan 5
@@ -30,6 +46,9 @@ DATE_FORMATS = [
     '%a, %d %b',  # Sun, 5 Jan
     '%d/%m/%Y',  # DD/MM/YYYY
 ]
-CLOCK_FORMATS = ['12h', '24h']
+CLOCK_FORMATS = [
+    '12h',
+    '24h'
+]
 TWELVE_HOURS_FORMAT = '%I:%M %p'  # eg. 11:38 PM
 TWENTY_FOUR_HOURS_FORMAT = '%H:%M'  # eg. 23:38
